f65fa38093a8a90b210700c573973913a7aaf689
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-19 14:09:26 +0200

    .

---
+++ SunamoEmbeddedResources/_sunamo/ThrowEx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunamoEmbeddedResources;
+internal class ThrowEx
+{
+    internal static void NotImplementedCase(List<char> generalChars)
+    {
+        throw new NotImplementedException();
+    }
+}
