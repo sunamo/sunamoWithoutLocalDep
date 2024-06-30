98d73dfe1811d654290e1780323ae8925d9bc9fb
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 15:45:31 +0200

    .

--- SunamoUriWebServices/UriWebServices.cs
+++ SunamoUriWebServices/UriWebServices.cs
@@ -1,192 +1,64 @@
 
 namespace SunamoUriWebServices;
 
-using System.Diagnostics;
-using System.Net;
-using System.Web;
 
 
-
-/// <summary>
-/// vše co je s adresou musím encodovat
-/// </summary>
 public partial class UriWebServices
 {
-    static int opened = 0;
-    public static string WikipediaEn = "https://en.wikipedia.org/w/index.php?search=%s";
-    public const string karaokeTexty = "http://www.karaoketexty.cz/search?q=%s&sid=bbrpp&x=36&y=9";
-
-    public const string instagramProfile = "https://www.instagram.com/{0}/";
-    public const string heureka = "https://www.heureka.cz/?h[fraze]=%s&ss=1";
-    public const string geocachingLog = "https://www.geocaching.com/play/geocache/%s/log";
-    public const string amateriComCs = "https://www.amateri.com/cs/lide/search?search=%s";
-    public const string amateriComEn = "https://www.amateri.com/en/lide/search?search=%s";
-
     /// <summary>
-    /// Insert A1 to every in A2 with %s
+    /// alphabetically
     /// </summary>
-    /// <param name="lyricsScz"></param>
-    /// <param name="clipboardL"></param>
-    public static void SearchAll(string lyricsScz, List<string> clipboardL)
+    static readonly List<string> githubRepos = SHGetLines.GetLines(@"GridControlsInWpf_Blog
+MyCodeExample
+sugo
+sunamo
+sunamo5
+SunamoCssGenerator
+SunamoEditorConfig
+SunamoLaTeX
+SunamoMathpix
+SunamoRobotsTxt
+SunamoRuleset
+sunamoWithoutLocalDep
+sunamoWithoutLocalDep5
+sunamoWithoutLocalDep.Tests
+sunamo.github.io
+sunamo.notmine
+sunamo.notmine5
+sunamo.notmine.Tests
+sunamo.performance
+sunamo.Tests
+sunamo.unsafe
+sunamo.unsafe.Tests
+sunpm
+TranslateEngine");
+
+    public const string githubCom = "https://github.com/";
+
+    public static bool IsGithubRepo(string fn)
     {
-        foreach (var item in clipboardL)
+        return githubRepos.Contains(fn);
+    }
+
+    public static string GitClone(string slnName)
+    {
+        return githubCom + "sunamo/" + slnName + ".git";
+    }
+
+    public static string AzureRepoWebUIFullOrGithub(string fn, AzureBuildUriArgs a = null)
+    {
+        if (IsGithubRepo(fn))
         {
-            opened++;
-            Process.Start(FromChromeReplacement(lyricsScz, item));
-
-            if (opened % 10 == 0)
-            {
-                System.Diagnostics.Debugger.Break();
-            }
+            return UriWebServices.GitClone(fn);
         }
+        return UriWebServices.AzureRepoWebUIFull(fn, a);
     }
 
-
-
-    public static void SearchAll(Func<string, string> topRecepty, List<string> clipboardL)
+    public partial class Facebook
     {
-
-        foreach (var item in clipboardL)
+        public static string FacebookProfile(string nick)
         {
-            opened++;
-            Process.Start(topRecepty.Invoke(item));
-            if (opened % 10 == 0)
-            {
-                System.Diagnostics.Debugger.Break();
-            }
+            return "https://www.facebook.com/" + nick;
         }
-    }
-
-
-
-    public static void GoogleSearch(List<string> list)
-    {
-        foreach (var item in list)
-        {
-            Process.Start(GoogleSearch(item));
-        }
-    }
-
-    public static string SpritMonitor(string car)
-    {
-        // https://www.spritmonitor.de/en/overview/45-Skoda/1289-Citigo.html?fueltype=4
-        string d = "cng overview -\"/detail/\"" + car;
-        return GoogleSearchSite("spritmonitor.de", d);
-    }
-
-
-
-    public static string SearchGitHub(string item)
-    {
-        return "https://github.com/search?q=" + item;
-    }
-
-    public static string WebShare(string item)
-    {
-        return "https://webshare.cz/#/search?what=" + UrlEncode(item);
-    }
-
-
-
-    public static string GooglePlusProfile(string nick)
-    {
-        return "https://www.google.com/" + nick;
-    }
-
-    public static void GoogleSearchInAllSite(List<string> allRepairKitShops, string v)
-    {
-        foreach (var item in allRepairKitShops)
-        {
-            if (opened % 10 == 0 && opened != 0)
-            {
-                //System.Diagnostics.Debugger.Break();
-#if DEBUG
-
-#endif
-
-            }
-            var uri = GoogleSearchSite(item, v);
-            Process.Start(uri);
-            opened++;
-        }
-    }
-
-    //http://www.bdsluzby.cz/stavebni-cinnost/materialy.htm
-
-
-
-    /// <summary>
-    /// A1 už musí být escapováno
-    /// </summary>
-    /// <param name="s"></param>
-    public static string GoogleSearch(string s)
-    {
-        // q for reviews in czech and not translated
-        return "https://www.google.cz/search?hl=cs&q=" + UrlEncode(s);
-    }
-
-    public static string GoogleSearchImages(string s)
-    {
-        // q for reviews in czech and not translated
-        return "https://www.google.cz/search?hl=cs&tbm=isch&q=" + UrlEncode(s);
-    }
-
-    public static string GoogleSearchSite(string site, string v)
-    {
-        site = site.Trim();
-
-        var uri = new Uri(site);
-
-        var host = string.Empty;
-        if (uri != null)
-        {
-            host = uri.Host;
-        }
-        else
-        {
-            host = site.ToString();
-        }
-
-        //https://www.google.cz/search?q=site%3Asunamo.cz+s
-        return "https://www.google.cz/search?q=site%3A" + host + "+" + UrlEncode(v);
-    }
-
-
-    /// <summary>
-    /// Point to Repos tab
-    /// Already new radekjancik
-    /// Working with spaces right (SQL Server Scripts1)
-    /// </summary>
-    /// <param name="slnName"></param>
-    public static string GitRepoInVsts(string slnName)
-    {
-        return "https://radekjancik.visualstudio.com/_git/" + WebUtility.UrlEncode(slnName);
-    }
-
-    /// <summary>
-    /// Just gray screen (22-12-2021)
-    /// </summary>
-    /// <param name="slnName"></param>
-    /// <returns></returns>
-    public static string AzureRepoWebUIFull2(string slnName)
-    {
-        return null;
-        // var enc = WebUtility.UrlEncode(slnName);
-        // return $"https://radekjancik@dev.azure.com/radekjancik/{enc}/_git/{enc}";
-    }
-
-    public static string AzureRepoWebUI(string slnName, AzureBuildUriArgs a = null)
-    {
-        return AzureRepoWebUIDomain(a) + WebUtility.UrlEncode(slnName);
-    }
-
-    public static string AzureRepoWebUISettings(string slnName)
-    {
-        return AzureRepoWebUI(slnName) + "/_settings/";
-    }
-
-    public static string UrlEncode(string s)
-    {
-        return HttpUtility.UrlEncode(s);
     }
 }
